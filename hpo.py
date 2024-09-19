e34d5747c52f0a8c4e6c05180f6e1d0cf0821604
Ankur Mishra <2018ankurm@gmail.com>
2024-09-19 13:07:14 -0400

    cleanup the hpo to use argparse instead of hydra

--- hpo.py
+++ hpo.py
@@ -1,19 +1,12 @@
-import os
-import multiprocessing
-import jax
-import training_io
+import sys
+import argparse
 from clearml import Task
 from clearml.automation import HyperParameterOptimizer
 from clearml.automation.optuna import OptimizerOptuna
 from clearml.automation.parameters import LogUniformParameterRange
-from train import Config, main_contained, get_model_name
-import dataclasses
-import hydra
-import jax_extra
-import subprocess
 
 
-def create_optimizer(base_task_id: str, config: Config):
+def create_optimizer(base_task_id: str, config: dict):
     return HyperParameterOptimizer(
         base_task_id=base_task_id,
         hyper_parameters=[
@@ -28,11 +21,11 @@ def create_optimizer(base_task_id: str, config: Config):
         objective_metric_series="loss",
         objective_metric_sign="min_global",
         optimizer_class=OptimizerOptuna,
-        execution_queue=config.training.queue,
+        execution_queue="v4-32",
         max_number_of_concurrent_tasks=1,  # 100 in the paper
         total_max_jobs=200,  # 800 in the paper
         min_iteration_per_job=1,
-        max_iteration_per_job=config.training.steps,
+        max_iteration_per_job=config["steps"],
     )
 
 
@@ -50,42 +43,29 @@ def job_complete_callback(
             objective_value))
 
 
-@ hydra.main(config_path="configs", version_base=None)
-def main(config):
-    config = jax_extra.make_dataclass_from_dict(Config, config)
-    if not config.training.queue:
-        raise ValueError("Training queue must be specified")
+def main():
+    parser = argparse.ArgumentParser(
+        description='Run optimization with a specified base task ID.')
+    parser.add_argument('--task_id', type=str,
+                        help='The base task ID (required)')
+    args = parser.parse_args()
 
-    # def run_base_task(queue):
-    #     base_task_id = create_base_task(config)
-    #     queue.put(base_task_id)
+    if args.task_id is None:
+        parser.print_help()
+        sys.exit(1)
 
-    # Create a queue to get the result from the subprocess
-    # result_queue = multiprocessing.Queue()
-
-    # # Start the base task creation in a separate process
-    # process = multiprocessing.Process(
-    #     target=run_base_task, args=(result_queue))
-    # process.start()
-
-    # # Wait for the base task ID
-    # base_task_id = result_queue.get()
-    # process.join()
-
-    base_task_id = "59cadf6662194d4dac383dfd5cd3e099"
-    print(base_task_id)
+    base_task_id = args.task_id
     base_task: Task = Task.get_task(base_task_id)
     project_name, task_name = base_task.get_project_name(), base_task.name
-
-    print(project_name, task_name)
-
+    config = base_task.get_configuration_object_as_dict('OmegaConf')
+    print(f"Using task ID: {base_task_id}")
+    print(f"Project name: {project_name}, Task name: {task_name}")
     task: Task = Task.init(
         project_name=f"{project_name}/hpo",
         task_name=f'hpo_{task_name}_gamma_sweep',
         task_type=Task.TaskTypes.optimizer,
         reuse_last_task_id=False
     )
-
     optim = create_optimizer(base_task_id, config)
     # report every 12 mins
     optim.set_report_period(12)

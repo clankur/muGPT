5219a787b980e21daeda6a3929bcc1ccd6afc6c6
Ankur Mishra <2018ankurm@gmail.com>
2024-10-29 18:12:48 -0400

    add basic load for llama

---
+++ load_models.py
@@ -0,0 +1,166 @@
+# %%
+import torch
+import numpy as np
+import jax
+from dlpack import asdlpack
+from jax.sharding import Mesh
+import jax.numpy as jnp
+from einops import rearrange, einsum
+import json
+import os
+from importlib import reload
+from typing import Optional
+from train import Hparams
+
+# %%
+model_path = os.path.expanduser("~/.llama/checkpoints/Llama3.2-1B/")
+with open(f"{model_path}/params.json", "r") as f:
+    params = json.load(f)
+weights = torch.load(
+    f"{model_path}/consolidated.00.pth", map_location=torch.device("cpu")
+)
+params
+
+# %%
+hidden_dim = 4 * params["dim"]
+hidden_dim = int(2 * hidden_dim / 3)
+hidden_dim = int(params["ffn_dim_multiplier"] * hidden_dim)
+hidden_dim = params["multiple_of"] * (
+    (hidden_dim + params["multiple_of"] - 1) // params["multiple_of"]
+)
+
+h = Hparams(
+    d_model=params["dim"],
+    n_q_per_kv=params["n_heads"] // params["n_kv_heads"],
+    n_kv=params["n_kv_heads"],
+    d_head=params["dim"] // params["n_heads"],
+    layers=params["n_layers"],
+    vocab=params["vocab_size"],
+    d_ff=hidden_dim,
+    rope_max_timescale=params["rope_theta"],
+    # TODO: add to Hparams
+    norm_eps=params["norm_eps"],
+    use_scale=params["use_scaled_rope"],
+)
+
+
+# %%
+# TODO: To load llama weights, model needs to flip n_q_per_kv n_kv
+# TODO: Implement a custom RoPE for llama
+def load_llama(weights, h: Hparams):
+    pre_attention_norms = []
+    pre_ffw_norms = []
+    attn_qs = []
+    attn_kvs = []
+    attn_os = []
+    mlp_gates = []
+    mlp_ups = []
+    mlp_downs = []
+
+    embed = jnp.from_dlpack(asdlpack(weights["tok_embeddings.weight"].float()))
+    unembed = jnp.from_dlpack(asdlpack(weights["tok_embeddings.weight"].float()))
+    final_norm = jnp.from_dlpack(asdlpack(weights["norm.weight"].float()))
+
+    for layer_id in range(h.layers):
+        # norms
+        ln1 = jnp.from_dlpack(
+            asdlpack(weights[f"layers.{layer_id}.attention_norm.weight"].float())
+        )
+        ln2 = jnp.from_dlpack(
+            asdlpack(weights[f"layers.{layer_id}.ffn_norm.weight"].float())
+        )
+        pre_attention_norms.append(ln1)
+        pre_ffw_norms.append(ln2)
+
+        # attention weights
+        w_q = jnp.from_dlpack(
+            asdlpack(weights[f"layers.{layer_id}.attention.wq.weight"].float()),
+        )
+
+        w_q = rearrange(
+            w_q,
+            "(n_kv n_q_per_kv d_head) d_model  -> n_kv n_q_per_kv d_head d_model",
+            n_q_per_kv=h.n_q_per_kv,
+            n_kv=h.n_kv,
+            d_head=h.d_head,
+        )  # d_model n_kv n_q_per_kv d_head
+
+        attn_qs.append(w_q)
+
+        w_k = jnp.from_dlpack(
+            asdlpack(weights[f"layers.{layer_id}.attention.wk.weight"].float()),
+        )
+
+        # rearranging dims like n_kv can cause issues!
+        w_k = rearrange(
+            w_k,
+            "(n_kv d_head) d_model -> d_model n_kv d_head",
+            n_kv=h.n_kv,
+            d_head=h.d_head,
+        )  # M_dim n_kv H_dim
+
+        w_v = jnp.from_dlpack(
+            asdlpack(weights[f"layers.{layer_id}.attention.wv.weight"].float()),
+        )
+
+        w_v = rearrange(
+            w_v,
+            "(n_kv d_head) d_model -> d_model n_kv d_head",
+            n_kv=h.n_kv,
+            d_head=h.d_head,
+        )  # M_dim n_kv H_dim
+        w_kv = jnp.stack([w_k, w_v], axis=0)
+        attn_kvs.append(w_kv)
+
+        w_o = jnp.from_dlpack(
+            asdlpack(weights[f"layers.{layer_id}.attention.wo.weight"].float()),
+        )
+        w_o = rearrange(
+            w_o,
+            "d_model (n_kv n_q_per_kv d_head) -> d_model n_kv n_q_per_kv d_head",
+            n_q_per_kv=h.n_q_per_kv,
+            n_kv=h.n_kv,
+            d_head=h.d_head,
+        )  # "d_model/d n_q_per_kv n_kv/t d_head"
+        attn_os.append(w_o)
+
+        # mlp
+        w_gate = jnp.from_dlpack(
+            asdlpack(weights[f"layers.{layer_id}.feed_forward.w1.weight"].float()),
+        )
+        w_gate = rearrange(w_gate, "d_ff d_model -> d_model d_ff")
+        mlp_gates.append(w_gate)
+
+        w_up = jnp.from_dlpack(
+            asdlpack(weights[f"layers.{layer_id}.feed_forward.w3.weight"].float()),
+        )
+        w_up = rearrange(w_up, "d_ff d_model -> d_model d_ff")
+        mlp_ups.append(w_up)
+
+        w_down = jnp.from_dlpack(
+            asdlpack(weights[f"layers.{layer_id}.feed_forward.w2.weight"].float()),
+        )
+        mlp_downs.append(w_down)
+
+    pre_attention_norms = jnp.stack(pre_attention_norms, axis=0)
+    pre_ffw_norms = jnp.stack(pre_ffw_norms, axis=0)
+    attn_qs = jnp.stack(attn_qs, axis=0)
+    attn_kvs = jnp.stack(attn_kvs, axis=0)
+    attn_os = jnp.stack(attn_os, axis=0)
+    mlp_gates = jnp.stack(mlp_gates, axis=0)
+    mlp_ups = jnp.stack(mlp_ups, axis=0)
+    mlp_downs = jnp.stack(mlp_downs, axis=0)
+
+    return (
+        embed,
+        unembed,
+        pre_attention_norms,
+        pre_ffw_norms,
+        attn_qs,
+        attn_kvs,
+        attn_os,
+        mlp_gates,
+        mlp_ups,
+        mlp_downs,
+        final_norm,
+    )

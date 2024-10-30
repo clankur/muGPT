38221d3467e46df1d86fea9e50d998041db4e0ea
Ankur Mishra <2018ankurm@gmail.com>
2024-10-30 11:44:59 -0400

    formalize loading into a state

--- load_models.py
+++ load_models.py
@@ -1,67 +1,64 @@
 # %%
 import torch
-import numpy as np
 import jax
-from dlpack import asdlpack
-from jax.sharding import Mesh
 import jax.numpy as jnp
-from einops import rearrange, einsum
+from dlpack import asdlpack
+from einops import rearrange
 import json
 import os
-from importlib import reload
-from typing import Optional
-from train import Hparams, BaseWidths
-
-# %%
-model_path = os.path.expanduser("~/.llama/checkpoints/Llama3.2-1B/")
-with open(f"{model_path}/params.json", "r") as f:
-    params = json.load(f)
-weights = torch.load(
-    f"{model_path}/consolidated.00.pth", map_location=torch.device("cpu")
-)
-params
-
-# %%
-hidden_dim = int(8 * params["dim"] // 3 * params["ffn_dim_multiplier"])
-hidden_dim = params["multiple_of"] * (
-    (hidden_dim + params["multiple_of"] - 1) // params["multiple_of"]
-)
-
-base = BaseWidths(
-    d_model=params["dim"],
-    n_q_per_kv=params["n_heads"] // params["n_kv_heads"],
-    n_kv=params["n_kv_heads"],
-    d_head=params["dim"] // params["n_heads"],
-    d_ff=hidden_dim,
-)
-
-h = Hparams(
-    d_model=params["dim"],
-    n_q_per_kv=params["n_heads"] // params["n_kv_heads"],
-    n_kv=params["n_kv_heads"],
-    d_head=params["dim"] // params["n_heads"],
-    layers=params["n_layers"],
-    vocab=params["vocab_size"],
-    d_ff=hidden_dim,
-    rope_max_timescale=params["rope_theta"],
-    norm_eps=params["norm_eps"],
-    use_scaled_rope=params["use_scaled_rope"],
-    # default other parameters from exp scaling/muP
-    base=base,
-    a_attn=1.0,
-    a_output=1.0,
-    zero_queries=False,
-    zero_unembed=False,
-    parameterization="sp",
-    fully_aligned=False,
-    gamma_embed=1.0,
-    gamma_hidden=1.0,
-    gamma_unembed=1.0,
-)
+from train import Hparams, BaseWidths, Model, State
+from typing import Tuple
 
 
 # %%
-def load_llama(weights, h: Hparams):
+def load_model_weights_and_hparams(model_path: str) -> Tuple[dict, Hparams]:
+    with open(f"{model_path}/params.json", "r") as f:
+        params = json.load(f)
+    weights = torch.load(
+        f"{model_path}/consolidated.00.pth", map_location=torch.device("cpu")
+    )
+
+    hidden_dim = int(8 * params["dim"] // 3 * params["ffn_dim_multiplier"])
+    hidden_dim = params["multiple_of"] * (
+        (hidden_dim + params["multiple_of"] - 1) // params["multiple_of"]
+    )
+
+    base = BaseWidths(
+        d_model=params["dim"],
+        n_q_per_kv=params["n_heads"] // params["n_kv_heads"],
+        n_kv=params["n_kv_heads"],
+        d_head=params["dim"] // params["n_heads"],
+        d_ff=hidden_dim,
+    )
+
+    h = Hparams(
+        d_model=params["dim"],
+        n_q_per_kv=params["n_heads"] // params["n_kv_heads"],
+        n_kv=params["n_kv_heads"],
+        d_head=params["dim"] // params["n_heads"],
+        layers=params["n_layers"],
+        vocab=params["vocab_size"],
+        d_ff=hidden_dim,
+        rope_max_timescale=params["rope_theta"],
+        norm_eps=params["norm_eps"],
+        use_scaled_rope=params["use_scaled_rope"],
+        # default other parameters from exp scaling/muP
+        base=base,
+        a_attn=1.0,
+        a_output=1.0,
+        zero_queries=False,
+        zero_unembed=False,
+        parameterization="sp",
+        fully_aligned=False,
+        gamma_embed=1.0,
+        gamma_hidden=1.0,
+        gamma_unembed=1.0,
+    )
+    return weights, h
+
+
+# %%
+def load_llama(weights: dict, h: Hparams) -> State:
     pre_attention_norms = []
     pre_ffw_norms = []
     attn_qs = []
@@ -97,7 +94,7 @@ def load_llama(weights, h: Hparams):
             n_q_per_kv=h.n_q_per_kv,
             n_kv=h.n_kv,
             d_head=h.d_head,
-        )  # d_model n_kv n_q_per_kv d_head
+        )  # n_kv n_q_per_kv H_dim M_dim
 
         attn_qs.append(w_q)
 
@@ -105,7 +102,6 @@ def load_llama(weights, h: Hparams):
             asdlpack(weights[f"layers.{layer_id}.attention.wk.weight"].float()),
         )
 
-        # rearranging dims like n_kv can cause issues!
         w_k = rearrange(
             w_k,
             "(n_kv d_head) d_model -> d_model n_kv d_head",
@@ -135,7 +131,7 @@ def load_llama(weights, h: Hparams):
             n_q_per_kv=h.n_q_per_kv,
             n_kv=h.n_kv,
             d_head=h.d_head,
-        )  # "d_model/d n_q_per_kv n_kv/t d_head"
+        )  # "M_dim n_kv n_q_per_kv H_dim"
         attn_os.append(w_o)
 
         # mlp
@@ -165,19 +161,27 @@ def load_llama(weights, h: Hparams):
     mlp_ups = jnp.stack(mlp_ups, axis=0)
     mlp_downs = jnp.stack(mlp_downs, axis=0)
 
-    return (
-        embed,
-        unembed,
-        pre_attention_norms,
-        pre_ffw_norms,
-        attn_qs,
-        attn_kvs,
-        attn_os,
-        mlp_gates,
-        mlp_ups,
-        mlp_downs,
-        final_norm,
+    weights = Model(
+        embed=embed,
+        unembed=unembed,
+        ln1=pre_attention_norms,
+        ln2=pre_ffw_norms,
+        w_q=attn_qs,
+        w_kv=attn_kvs,
+        w_o=attn_os,
+        w_gate=mlp_gates,
+        w_up=mlp_ups,
+        w_down=mlp_downs,
+        final_layer_norm=final_norm,
     )
+    adam_mu = jax.tree.map(lambda p: p * 0.0, weights)
+    adam_nu = jax.tree.map(lambda p: p * 0.0, weights)
+    return State(weights=weights, adam_mu=adam_mu, adam_nu=adam_nu)
 
+
+# %%
+model_path = os.path.expanduser("~/.llama/checkpoints/Llama3.2-1B/")
+weights, h = load_model_weights_and_hparams(model_path)
+state = load_llama(weights, h)
 
 # %%

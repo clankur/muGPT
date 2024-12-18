94c9477f169391949bbe360a96308681eaaa82d7
Ankur Mishra <2018ankurm@gmail.com>
2024-12-17 20:46:38 -0500

    write up rough implementation of alibi

--- train.py
+++ train.py
@@ -631,6 +631,23 @@ class Cope:
         return logits + out
 
 
+def get_alibi_bias(logits: f32["B/d Qlen Klen Q K/t"]) -> f32["1 Qlen Klen 1 K/t"]:
+    _, Qlen, _, _, n_kv = logits.shape
+    start = 2.0 ** (-(2.0 ** -(jnp.log2(n_kv) - 3)))
+    # TODO: change this to a class, and make it a parameter
+    slopes = start * (start ** jnp.arange(0, n_kv))
+    slopes = einops.rearrange(slopes, "K -> 1 1 1 K")
+
+    position_bias = jnp.arange(Qlen)[None, :] - jnp.arange(Qlen)[:, None]
+    position_bias = einops.rearrange(position_bias, "Qlen Klen -> Qlen Klen 1 1")
+
+    bias: f32["1 Qlen Klen 1 K/t"] = einops.rearrange(
+        position_bias * slopes, "Qlen Klen 1 K -> 1 Qlen Klen 1 K"
+    )
+
+    return bias
+
+
 @pytree_dataclass
 class RopeTable:
     sin: f32["len d_head2"]
